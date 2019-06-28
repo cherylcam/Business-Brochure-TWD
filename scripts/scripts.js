f6aa29d25c413583ab2a4e216d80918b1a2f43ca
chanroyc <chanroyc@gmail.com>
2019-06-27 22:31:24 -0700

    Added Automatic Slideshow Alpha
    Added automatic slideshow functionality. Partially Complete.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -17,3 +17,25 @@ const $next = document.getElementById('slideshow-next');
 $('#slideshow').click(function(){
     $prev.classList.click
 });
+
+var slideIndex=0;
+var $slides = $('.slides');
+showSlides();
+
+function showSlides(){
+    console.log(slideIndex);
+    for (i=0; i<$slides.length; i++){
+        $slides[i].style.display="none";
+    }
+
+    if(slideIndex == $slides.length){
+        slideIndex = 0;
+        $slides[slideIndex].style.display = "block";
+        slideIndex++;
+        setTimeout(showSlides,1000);
+    }else{
+        $slides[slideIndex].style.display = "block";
+        slideIndex++;
+        setTimeout(showSlides, 3000);
+    }
+}
