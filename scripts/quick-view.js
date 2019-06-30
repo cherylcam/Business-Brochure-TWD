16db440b17dc4bc9748662195067c4c8729e4365
chanroyc <chanroyc@gmail.com>
2019-06-29 19:20:32 -0700

    Added Gallery to Popup on Acoustic/Electric Pages
    Added Gallery for front and backsides of guitars for popups on acoustic and electric guitar pages. Updated image names and image sizing of guitars.

--- scripts/quick-view.js
+++ scripts/quick-view.js
@@ -8,4 +8,15 @@ $(".popup").click(function(){
 $(".close-quick-view").click(function(){
     console.log($(this).parent());
     $(this).parent().fadeOut();
-})
+});
+
+$('.gallery-img').click(function(){
+    console.log($(this).children());
+    if ($(this).children().attr('class') === $('.thumb-front').attr('class')){
+        $('.guitar-front').fadeIn();
+        $('.guitar-back').css('display', 'none');
+    }else{
+        $('.guitar-back').fadeIn();
+        $('.guitar-front').css('display', 'none');
+    };
+});
