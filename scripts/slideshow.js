60305af2c64873adbeb1332fa01b492a284faf29
chanroyc <chanroyc@gmail.com>
2019-07-02 13:55:02 -0700

    Updated Slideshow

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -4,6 +4,9 @@ var slideIndex=0;
 var $slides = $('.slides');
 var $dot = $('.dot');
 var timer = null;
+var start = true;
+var startPrev = true;
+var startNext = false;
 
 showSlides();
 
@@ -45,32 +48,56 @@ $('.dot').click(function(){
 });
 
 function showSlides(){
-    for (i=0; i<$slides.length; i++){
-        $slides[i].style.display="none";
-        $('.dot').eq(i).css('background-color', 'white')
+    $('.dot').css('background-color', 'white');
+    startPrev = true;
+
+    if (startNext === true){
+        slideIndex++;
+        startNext = false;
     }
 
     if(slideIndex === $slides.length){
         slideIndex = 0;
     }  
-    $slides.eq(slideIndex).fadeIn(1000);
+    
+    if (start === true){
+        $slides.hide();
+        $slides.eq(slideIndex).show();
+        start = false;
+    }else{
+        $slides.eq(slideIndex-1).hide('slide', {direction: 'left'});
+        $slides.eq(slideIndex).show('slide', {direction: 'right'});
+    }
+
     $('.dot').eq(slideIndex).css('background-color', '#660000');
-    slideIndex++;
+    
+    slideIndex++;   
+  
     timer = setTimeout(showSlides, 5000);
 };
 
 function prevSlide(){
-    slideIndex = slideIndex - 2;
-    for (i=0; i<$slides.length; i++){
-        $slides[i].style.display="none";
-        $('.dot').eq(i).css('background-color', 'white')
+    $('.dot').css('background-color', 'white');
+    startNext = true;
+
+    if (startPrev === true){
+        slideIndex = slideIndex - 2;
+        startPrev = false;
+    }else{
+        slideIndex--;
     }
 
     if(slideIndex < 0){
+        $slides.eq(0).hide('slide', {direction: 'right'});
         slideIndex = $slides.length - 1;
-    }  
-    $slides.eq(slideIndex).fadeIn(1000);
+    }else if (slideIndex === 1){
+        $slides.eq(slideIndex-1).hide('slide', {direction: 'right'});
+    }
+
+    $slides.eq(slideIndex+1).hide('slide', {direction: 'right'});
+    $slides.eq(slideIndex).show('slide', {direction:'left'});
+    
     $('.dot').eq(slideIndex).css('background-color', '#660000');
-    slideIndex++;
+
     timer = setTimeout(showSlides, 5000);
 };
